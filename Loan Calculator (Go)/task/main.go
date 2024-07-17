8bf4f54f6903f2c35baa65f4c6e121bbea42282e
Gleb Vinogradov <g.vinogradov@team.bumble.com>
2024-07-17 18:29:05 +0400

    [master] 2/4 Dreamworld
    Description
    
    If you found the previous stage too easy, let's add something interesting. The best loans are probably those with a 0% interest.
    
    Let's make some calculations for 0% loan repayments. The user might know the period of the loan and want to calculate the monthly payment. Or the user might know the amount of the monthly repayments and wonder how many months it will take to repay the loan in full.
    
    In this stage, we need to ask the user to input the loan principal amount. Then, the user should indicate what needs to be calculated (the monthly payment amount or the number of months) and enter the required parameter. After that, the loan calculator should output the value that the user wants to know.
    
    Also, let’s assume we don't care about decimal places. If you get a floating-point expression as a result of the calculation, you’ll have to do additional actions. Take a look at Example 4 where you need to calculate the monthly payment. You know that the loan principal is 1000 and want to pay it back in 9 months. The real value of payment can be calculated as
    
    Of course, you can't pay that amount of money since it's a float value. You have to round up this value and make it 112. But why do we round it up instead of rounding it down? Remember that no payment can be more than the fixed monthly payment. If your monthly repayment amount is 111, that would make the last payment 112, which is not acceptable. If you make a monthly payment of 112, the last payment will be 104, which is fine. You can calculate the last payment as follows
    
    In this stage, you need to count the number of months or the monthly payment. If the last payment differs from the rest, the program should display the monthly payment and the last payment.
    
    Objectives
    
    The behavior of your program should look like this:
    
    1. Prompt a user to enter their loan principal and choose which of the two parameters they want to calculate – the number of monthly payments or the monthly payment amount.
    2. To perform further calculations, you'll also have to ask for the required missing value.
    3. Finally, output the results for the user.

--- Loan Calculator (Go)/task/main.go
+++ Loan Calculator (Go)/task/main.go
@@ -1,16 +1,77 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+type calculator struct {
+	loanPrincipal  float64
+	months         int
+	monthlyPayment float64
+}
+
+func (c *calculator) calculateMonths() {
+	months := c.loanPrincipal / c.monthlyPayment
+	c.months = int(months)
+	if float64(int(months)) != months {
+		c.months++
+	}
+}
+
+func (c *calculator) calculateMonthlyPayment() {
+	c.monthlyPayment = math.Ceil(c.loanPrincipal / float64(c.months))
+}
+
+func (c *calculator) getLastMonthPayment() float64 {
+	return c.loanPrincipal - c.monthlyPayment*float64(c.months-1)
+}
 
 func main() {
-	var (
-		loanPrincipal = "Loan principal: 1000"
-		firstMonth    = "Month 1: repaid 250"
-		secondMonth   = "Month 2: repaid 250"
-		thirdMonth    = "Month 3: repaid 500"
-		finalOutput   = "The loan has been repaid!"
-	)
+	calculator := calculator{}
 
-	// Write your code solution for the project below.
-	fmt.Printf("%s\n%s\n%s\n%s\n%s\n", loanPrincipal, firstMonth, secondMonth, thirdMonth, finalOutput)
+	fmt.Println("Enter the loan principal:")
+	fmt.Scan(&calculator.loanPrincipal)
+
+	fmt.Println(
+		"What do you want to calculate?\n" +
+			"type \"m\" for number of monthly payments,\n" +
+			"type \"p\" for the monthly payment:",
+	)
+	var option string
+	fmt.Scan(&option)
+
+	switch option {
+	case "m":
+		processNumberOfMonthlyPayments(calculator)
+	case "p":
+		processMontlyPayment(calculator)
+	default:
+		fmt.Println("Incorrect option")
+	}
+}
+
+func processNumberOfMonthlyPayments(calculator calculator) {
+	fmt.Println("Enter the monthly payment:")
+	fmt.Scan(&calculator.monthlyPayment)
+	calculator.calculateMonths()
+
+	if calculator.months > 1 {
+		fmt.Printf("It will take %d months to repay the loan\n", calculator.months)
+	} else {
+		fmt.Printf("It will take 1 month to repay the loan\n")
+	}
+}
+
+func processMontlyPayment(calculator calculator) {
+	fmt.Println("Enter the number of months:")
+	fmt.Scan(&calculator.months)
+	calculator.calculateMonthlyPayment()
+
+	lastMonthPayment := calculator.getLastMonthPayment()
+	if lastMonthPayment != calculator.monthlyPayment {
+		fmt.Printf("Your monthly payment = %0.f and the last payment = %0.f.\n", calculator.monthlyPayment, lastMonthPayment)
+	} else {
+		fmt.Printf("Your monthly payment = %0.f\n", calculator.monthlyPayment)
+	}
 }
