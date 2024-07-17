ca6eea79f04d310cb4e85b9a1805c215abd573bf
Gleb Vinogradov <g.vinogradov@team.bumble.com>
2024-07-17 17:50:55 +0400

    [master] project init

---
+++ Loan Calculator (Go)/task/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		loanPrincipal = "Loan principal: 1000"
+		firstMonth    = "Month 1: repaid 250"
+		secondMonth   = "Month 2: repaid 250"
+		thirdMonth    = "Month 3: repaid 500"
+		finalOutput   = "The loan has been repaid!"
+	)
+
+	// Write your code solution for the project below.
+	fmt.Println("Hello, World!")
+}
