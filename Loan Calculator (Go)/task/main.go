c5ee82da992f084ffc22ef5225d4dbdde8022e12
Gleb Vinogradov <g.vinogradov@team.bumble.com>
2024-07-18 13:31:14 +0400

    [master] 3/4 Annuity payment
    Description
    
    Let's compute all the parameters of the loan. There are at least two kinds of loan: those with annuity payment and those with differentiated payment. In this stage, you are going to calculate only the former. Annuity type of payment consists in paying a fixed sum of money at specified intervals, for example, each month or each year. The annuity payment amount is precisely this fixed sum of money that you need to pay at regular intervals.
    
    Objectives
    
    In this stage, you should change the behavior of the calculator:
    
    1. First, you should parse the provided parameters to define, which ones are known and which one is missing using the flag package.
    2. Compute the missing value using the formulas mentioned above. The calculator should be able to calculate the number of monthly payments, the monthly payment amount, and the loan principal.
    3. Finally, output the value users wanted to compute.
    
    The final version of your program is supposed to work from the command line and parse the following arguments:
    
        --payment is the payment amount. It can be calculated using the provided principal, interest, and number of months
        --principal You can get its value if you know the interest, annuity payment, and number of months.
        --periods denotes the number of months needed to repay the loan. It's calculated based on the interest, annuity payment, and principal.
        --interest is specified without a percent sign. Note that it can accept a floating-point value. Our loan calculator can't calculate the interest, so it must always be provided.

--- Loan Calculator (Go)/task/main.go
+++ Loan Calculator (Go)/task/main.go
@@ -1,73 +1,115 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"math"
 )
 
 type calculator struct {
-	loanPrincipal  float64
-	months         float64
-	monthlyPayment float64
+	principal        float64
+	numberOfPayments float64
+	payment          float64
+	interest         float64
 }
 
-func (c *calculator) calculateMonths() {
-	c.months = math.Ceil(c.loanPrincipal / c.monthlyPayment)
+// Convert annual interest rate to monthly and to a decimal
+func (c *calculator) getMonthlyInterestRate() float64 {
+	return c.interest / (12 * 100)
 }
 
-func (c *calculator) calculateMonthlyPayment() {
-	c.monthlyPayment = math.Ceil(c.loanPrincipal / c.months)
+func (c *calculator) calculateNumberOfPayments() {
+	i := c.getMonthlyInterestRate()
+	c.numberOfPayments = math.Ceil(math.Log(c.payment/(c.payment-i*c.principal)) / math.Log(1+i))
 }
 
-func (c *calculator) getLastMonthPayment() float64 {
-	return c.loanPrincipal - c.monthlyPayment*(c.months-1.00)
+func (c *calculator) calculatePayment() {
+	i := c.getMonthlyInterestRate()
+	c.payment = math.Ceil(c.principal * (i * math.Pow(1+i, c.numberOfPayments)) / (math.Pow(1+i, c.numberOfPayments) - 1))
+}
+
+func (c *calculator) getLastPayment() float64 {
+	return c.principal - c.payment*(c.numberOfPayments-1.00)
+}
+
+func (c *calculator) calculatePrincipal() {
+	i := c.getMonthlyInterestRate()
+	c.principal = c.payment * (math.Pow(1+i, c.numberOfPayments) - 1) / (i * math.Pow(1+i, c.numberOfPayments))
 }
 
 func main() {
-	calculator := calculator{}
+	initValue := 0.00
+	calc := calculator{}
 
-	fmt.Println("Enter the loan principal:")
-	fmt.Scan(&calculator.loanPrincipal)
+	flag.Float64Var(&calc.principal, "principal", initValue, "loan principal")
+	flag.Float64Var(&calc.payment, "payment", initValue, "loan principal")
+	flag.Float64Var(&calc.numberOfPayments, "periods", initValue, "loan principal")
+	flag.Float64Var(&calc.interest, "interest", initValue, "loan principal")
+	flag.Parse()
 
-	fmt.Println(
-		"What do you want to calculate?\n" +
-			"type \"m\" for number of monthly payments,\n" +
-			"type \"p\" for the monthly payment:",
-	)
-	var option string
-	fmt.Scan(&option)
-
-	switch option {
-	case "m":
-		processNumberOfMonthlyPayments(calculator)
-	case "p":
-		processMonthlyPayment(calculator)
-	default:
-		fmt.Println("Incorrect option")
+	switch initValue {
+	case calc.payment:
+		processMonthlyPayment(calc)
+	case calc.principal:
+		processPrincipal(calc)
+	case calc.numberOfPayments:
+		processNumberOfMonthlyPayments(calc)
+	case calc.interest:
+		log.Fatal("Not yet implemented")
 	}
 }
 
-func processNumberOfMonthlyPayments(calculator calculator) {
-	fmt.Println("Enter the monthly payment:")
-	fmt.Scan(&calculator.monthlyPayment)
-	calculator.calculateMonths()
+func processNumberOfMonthlyPayments(calc calculator) {
+	// perform calculation
+	calc.calculateNumberOfPayments()
+	// display result
 
-	if calculator.months > 1 {
-		fmt.Printf("It will take %d months to repay the loan\n", calculator.months)
+	// It will take 8 years and 2 months to repay this loan!
+	years := math.Floor(calc.numberOfPayments / 12)
+	months := calc.numberOfPayments - 12*years
+
+	if years == 0 {
+		if months > 1 {
+			fmt.Printf("It will take %0.f months to repay the loan\n", months)
+		} else {
+			fmt.Printf("It will take 1 month to repay the loan\n")
+		}
+	} else if months == 0 {
+		if years > 1 {
+			fmt.Printf("It will take %0.f years to repay the loan\n", years)
+		} else {
+			fmt.Printf("It will take 1 year to repay the loan\n")
+		}
 	} else {
-		fmt.Printf("It will take 1 month to repay the loan\n")
+		yearsPart, monthsPart := fmt.Sprintf("%0.f years", years), fmt.Sprintf("%0.f months", months)
+		if years == 1 {
+			yearsPart = "1 year"
+		}
+		if months == 1 {
+			monthsPart = "1 month"
+		}
+
+		fmt.Printf("It will take %s and %s to repay the loan\n", yearsPart, monthsPart)
 	}
 }
 
-func processMonthlyPayment(calculator calculator) {
-	fmt.Println("Enter the number of months:")
-	fmt.Scan(&calculator.months)
-	calculator.calculateMonthlyPayment()
-
-	lastMonthPayment := calculator.getLastMonthPayment()
-	if lastMonthPayment != calculator.monthlyPayment {
-		fmt.Printf("Your monthly payment = %0.f and the last payment = %0.f.\n", calculator.monthlyPayment, lastMonthPayment)
+func processMonthlyPayment(calc calculator) {
+	// perform calculation
+	calc.calculatePayment()
+	// get last month payment
+	lastMonthPayment := calc.payment // calc.getLastPayment()
+	// display result
+	if lastMonthPayment != calc.payment {
+		fmt.Printf("Your monthly payment = %0.f and the last payment = %0.f.\n", calc.payment, lastMonthPayment)
 	} else {
-		fmt.Printf("Your monthly payment = %0.f\n", calculator.monthlyPayment)
+		fmt.Printf("Your monthly payment = %0.f!\n", calc.payment)
 	}
+}
+
+func processPrincipal(calc calculator) {
+	// perform calculation
+	calc.calculatePrincipal()
+	// display result
+	fmt.Printf("Your loan principal = %0.f!\n", calc.principal)
 }
